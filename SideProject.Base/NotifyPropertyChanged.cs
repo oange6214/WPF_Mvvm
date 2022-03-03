7c93624c9156a338184f5070777438004e31e0cc
柏翰 林 <his6214@hotmail.com>
2022-03-04 01:29:06 +0800

    Revert "Update"
    This reverts commit 6386566b95b7e2a3f4e5d5ae8a78bd3300322b1a.

--- SideProject.Base/NotifyPropertyChanged.cs
+++ SideProject.Base/NotifyPropertyChanged.cs
@@ -1,57 +1,13 @@
-﻿using System.Collections.Generic;
-using System.ComponentModel;
-using System.Runtime.CompilerServices;
+﻿using System.ComponentModel;
 
 namespace SideProject.Base
 {
     public class NotifyPropertyChanged : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
-
-        /// <summary>
-        /// 通知屬性更改
-        /// </summary>
-        /// <param name="propertyName"></param>
         protected void RaisePropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        /// <summary>
-        /// 通知屬性更改，可以不用每個屬性增加名稱
-        /// </summary>
-        /// <param name="propertyName"></param>
-        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-
-        /// <summary>
-        /// 集合
-        /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="field">私有字段</param>
-        /// <param name="value">設定值</param>
-        /// <param name="PropertyName"></param>
-        /// <returns></returns>
-        protected bool Set<T>(ref T field, T value, [CallerMemberName] string PropertyName = null)
-        {
-            if (EqualityComparer<T>.Default.Equals(field, value)) return false;
-
-            field = value;
-
-            RaisePropertyChanged(PropertyName);
-
-            return true;
-        }
-
-
-        /// <summary>
-        /// 通知 繼承的類別內部所有的屬性，進行更新。（重點在名稱沒有指定）
-        /// </summary>
-        protected void RaiseAllChanged()
-        {
-            RaisePropertyChanged("");
         }
     }
 }
