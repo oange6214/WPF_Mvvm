b43f3080a2c5762f5fc0ecfcef2a159e60d4f25b
柏翰 林 <his6214@hotmail.com>
2022-03-05 08:02:52 +0800

    Update NotifyPropertyChanged class

--- SideProject.Base/NotifyPropertyChanged.cs
+++ SideProject.Base/NotifyPropertyChanged.cs
@@ -1,13 +1,59 @@
-﻿using System.ComponentModel;
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
 
 namespace SideProject.Base
 {
+    /// <summary>
+    /// 通知屬性更改基礎類，ViewModel 需要通知界面時，需要繼承 <see cref="INotifyPropertyChanged"/>這個接口
+    /// </summary>
     public class NotifyPropertyChanged : INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangedEventHandler PropertyChanged = (s, e) => { };
+
+        /// <summary>
+        /// 通知屬性更改
+        /// </summary>
+        /// <param name="propertyName"></param>
         protected void RaisePropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        /// <summary>
+        /// 通知屬性更改，可自動抓取 Property Name
+        /// </summary>
+        /// <param name="propertyName"></param>
+        public void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            RaisePropertyChanged(propertyName);
+        }
+
+        /// <summary>
+        /// 利用泛型，簡化屬性 set 功能
+        /// </summary>
+        /// <typeparam name="T">屬性類型</typeparam>
+        /// <param name="field">字段</param>
+        /// <param name="value">設定值</param>
+        /// <param name="propertyName">屬性名稱</param>
+        /// <returns></returns>
+        protected bool Set<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if(EqualityComparer<T>.Default.Equals(field, value)) return false;
+
+            field = value;
+
+            RaisePropertyChanged(propertyName);
+
+            return true;
+        }
+
+        /// <summary>
+        /// 通知所有屬性更新（通知繼承當前類別所有的屬性）
+        /// </summary>
+        protected void RaiseAllChanged()
+        {
+            RaisePropertyChanged("");
         }
     }
 }
--- WPF_Mvvm/ViewModels/MainViewModel.cs
+++ WPF_Mvvm/ViewModels/MainViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using SideProject.Base;
 using System;
+using System.Collections.ObjectModel;
 using System.Windows;
+using WPF_Mvvm.Models;
 
 namespace WPF_Mvvm.ViewModels
 {
@@ -8,10 +10,7 @@ namespace WPF_Mvvm.ViewModels
     {
         public MainViewModel()
         {
-            SearchCommand = new RelayCommand(() =>
-            {
-                MessageBox.Show(SearchText);
-            });
+
         }
 
         private string _searchText;
@@ -26,7 +25,42 @@ namespace WPF_Mvvm.ViewModels
             }
         }
 
-        public RelayCommand SearchCommand { get; private set; }
+        public RelayCommand SearchCommand
+        {
+            get
+            {
+                return new RelayCommand(() =>
+                {
+                    MessageBox.Show(SearchText);
+                });
+            }
+        }
 
+        private ObservableCollection<IconModel> _iconModels;
+        /// <summary>
+        /// 方法一、Icon 資料集（自動取得屬性名稱）
+        /// </summary>
+        public ObservableCollection<IconModel> IconModels
+        {
+            get { return _iconModels; }
+            set 
+            {
+                _iconModels = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private ObservableCollection<IconModel> _iconModels2;
+        /// <summary>
+        /// 方法二、Icon 資料集（封裝 OnPropertyChanged）
+        /// </summary>
+        public ObservableCollection<IconModel> IconModels2
+        {
+            get { return _iconModels2; }
+            set
+            {
+                Set(ref _iconModels2, value);
+            }
+        }
     }
 }
