add51aaa0b9ecb590ac7185344cf6d0b26e5b470
柏翰 林 <his6214@hotmail.com>
2022-03-05 08:02:52 +0800

    Update

--- SideProject.Base/NotifyPropertyChanged.cs
+++ SideProject.Base/NotifyPropertyChanged.cs
@@ -2,9 +2,17 @@
 
 namespace SideProject.Base
 {
+    /// <summary>
+    /// 通知屬性更改基礎類，ViewModel 需要通知界面時，需要繼承 <see cref="INotifyPropertyChanged"/>這個接口
+    /// </summary>
     public class NotifyPropertyChanged : INotifyPropertyChanged
     {
-        public event PropertyChangedEventHandler PropertyChanged;
+        public event PropertyChangedEventHandler PropertyChanged = (s, e) => { };
+
+        /// <summary>
+        /// 通知屬性更改
+        /// </summary>
+        /// <param name="propertyName"></param>
         protected void RaisePropertyChanged(string propertyName)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
